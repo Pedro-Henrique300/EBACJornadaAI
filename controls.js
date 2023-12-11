059c83908a40be53dde099f0b2372e59442e9dbd
Pedro Henrique <pedrohenriquecamargo2019@gmail.com>
2023-12-10 21:54:11 -0300

    Códigos Completos

--- controls.js
+++ controls.js
@@ -1,5 +1,5 @@
 export default {
-    jump: new KeyboardEvent('kaydown', {key: 'Space', keyCode: 32}),
+    jump: new KeyboardEvent('keydown', {key: 'Space', keyCode: 32 }),
     dispatch(event) {
         document.dispatchEvent(this[event]);
     }
